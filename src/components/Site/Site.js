df34ff3f40661c4024c53442c1181baabb9786f8
Nick Hoang <nickhoang11@gmail.com>
2019-06-13 15:38:12 -0700

    Set up individual Site component.

---
+++ src/components/Site/Site.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const site = props => (
+  <article>
+    <div>Site Component</div>
+  </article>
+);
+
+export default site;
--- src/containers/Mapcanvas/Sites/Sites.js
+++ src/containers/Mapcanvas/Sites/Sites.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Site from '../../../components/Site/Site';
+
 class Sites extends Component {
   render() {
     return (
       <div>
         <section className="Sites">
+          <Site />
           <p>site 1</p>
           <p>site 2</p>
           <p>site 3</p>
