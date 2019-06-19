16e4d4cd5876e108308888d460b8d84c0cf9f894
Nick Hoang <nickhoang11@gmail.com>
2019-06-19 16:08:36 -0700

    Fix syntax error

--- src/components/MapCanvas.js
+++ src/components/MapCanvas.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 
+import summary from '../apis/summary';
 class MapCanvas extends Component {
+  state = {
+    geometry: {}
+  };
+
+  //here just test the onViewChange function for now
+  componentDidMount() {
+    this.onViewChange();
+  }
+
+  onViewChange = () => {
+    const data = {
+      geometry: this.state.geometry
+    };
+
+    summary.post('/sites', data).then(response => {
+      //just inital set up, response will filter and return all geometry feature here.
+      console.log(response);
+    });
+  };
+
   render() {
     return <div>MAP</div>;
   }
--- src/components/SiteDetail.js
+++ src/components/SiteDetail.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import summary from '../apis/summary';
-
 class SiteDetail extends Component {
   state = {
     loadedSites: null
