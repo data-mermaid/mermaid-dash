2b93fb7bc3cb050b1786eb706feeeb2d9d4113e8
Nick Hoang <nickhoang11@gmail.com>
2019-06-19 17:03:42 -0700

    Add semntic ui for css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>TOOLBAR</div>;
+  return (
+    <div className="ui inverted pointing menu">
+      <div className="item">MERMAID</div>
+      <div className="right menu">
+        <div className="item">COLLECT</div>
+        <div className="item">ABOUT THESE DATA</div>
+        <div className="item">PRIVACY</div>
+        <div className="item">CONTACT</div>
+        <div className="item">
+          <button className="ui inverted button">Show Full Map</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
