9153290ad1572022c5b216bfa75d8663d8410800
Nick Hoang <nickhoang11@gmail.com>
2019-06-21 16:23:43 -0700

    Add and experiementing react-map-gl wrapper

--- src/components/MapCanvas.js
+++ src/components/MapCanvas.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import { Icon } from 'semantic-ui-react';
 
 import summary from '../apis/summary';
+import './MapCanvas.css';
+
+const MAPBOX_TOKEN =
+  'pk.eyJ1Ijoibmlja2hvYW5nIiwiYSI6ImNqeDB3NzNzbjAzamg0Ym83aXZmcWEzbDcifQ.hvWWVoMPsWS2KVa8nFfOkA';
 
 class MapCanvas extends Component {
   state = {
-    geometry: {}
+    geometry: {},
+    viewport: {
+      width: '100vw',
+      height: '100vh',
+      latitude: -12.287,
+      longitude: 113.278,
+      zoom: 3
+    },
+    geoObject: null
   };
 
   componentDidMount() {
@@ -17,11 +30,36 @@ class MapCanvas extends Component {
       geometry: this.state.geometry
     };
     const { data: result } = await summary.post('/sites', geoData);
-    console.log(result);
+    this.setState({ geoObject: result });
   }
 
   render() {
-    return <Header as="h1">MAP</Header>;
+    const siteLocations = this.state.geoObject ? (
+      this.state.geoObject.features.map(site => (
+        <Marker
+          key={site.id}
+          latitude={site.geometry.coordinates[1]}
+          longitude={site.geometry.coordinates[0]}
+        >
+          <Icon name="circle" color="red" />
+        </Marker>
+      ))
+    ) : (
+      <div>NOT FOUND</div>
+    );
+
+    return (
+      <div className="map-overlay">
+        <ReactMapGL
+          {...this.state.viewport}
+          mapboxApiAccessToken={MAPBOX_TOKEN}
+          mapStyle="mapbox://styles/nickhoang/cjx6prmf703ev1cqz9qd1ykdr"
+          onViewportChange={viewport => this.setState({ viewport })}
+        >
+          {siteLocations}
+        </ReactMapGL>
+      </div>
+    );
   }
 }
 
