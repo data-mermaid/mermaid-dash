fe795fe23f3446302fe6b077bf16ad0ec34b0aef
Nick Hoang <nickhoang11@gmail.com>
2019-06-24 14:21:44 -0700

    Minor ux change for header toolbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Segment } from 'semantic-ui-react';
+import { Menu } from 'semantic-ui-react';
 
 import ToggleMap from './ToggleMap';
 
@@ -13,36 +13,34 @@ class Header extends Component {
 
     return (
       <div>
-        <Segment inverted>
-          <Menu inverted pointing secondary>
-            <Menu.Item name="MERMAID" />
-            <Menu.Menu position="right">
-              <Menu.Item
-                name="COLLECT"
-                active={activeItem === 'COLLECT'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="ABOUT THESE DATA"
-                active={activeItem === 'ABOUT THESE DATA'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="PRIVACY"
-                active={activeItem === 'PRIVACY'}
-                onClick={this.handleItemClick}
-              />
-              <Menu.Item
-                name="CONTACT"
-                active={activeItem === 'CONTACT'}
-                onClick={this.handleItemClick}
-              />
-            </Menu.Menu>
-            <Menu.Item>
-              <ToggleMap toggle={this.props.toggle} />
-            </Menu.Item>
-          </Menu>
-        </Segment>
+        <Menu inverted>
+          <Menu.Item name="MERMAID" />
+          <Menu.Menu position="right">
+            <Menu.Item
+              name="COLLECT"
+              active={activeItem === 'COLLECT'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="ABOUT THESE DATA"
+              active={activeItem === 'ABOUT THESE DATA'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="PRIVACY"
+              active={activeItem === 'PRIVACY'}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="CONTACT"
+              active={activeItem === 'CONTACT'}
+              onClick={this.handleItemClick}
+            />
+          </Menu.Menu>
+          <Menu.Item>
+            <ToggleMap toggle={this.props.toggle} />
+          </Menu.Item>
+        </Menu>
       </div>
     );
   }
