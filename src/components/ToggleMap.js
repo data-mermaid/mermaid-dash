c606376aa5ebc9d1bcd3753354c3edc304a3acdb
Nick Hoang <nickhoang11@gmail.com>
2019-06-24 14:21:19 -0700

    Add custom button

--- src/components/ToggleMap.js
+++ src/components/ToggleMap.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { ThemeProvider } from 'styled-components/macro';
+import { ButtonStyle } from './Button';
+
+const theme = {
+  fg: '#1B1C1D',
+  bg: 'white'
+};
+
+const invertTheme = ({ fg, bg }) => ({
+  fg: bg,
+  bg: fg
+});
 
 const toggleMap = props => (
-  <Button inverted onClick={props.toggle}>
-    Show Full Map
-  </Button>
+  <ThemeProvider theme={theme}>
+    <ThemeProvider theme={invertTheme}>
+      <ButtonStyle onClick={props.toggle} n>
+        Show Full Map{' '}
+      </ButtonStyle>
+    </ThemeProvider>
+  </ThemeProvider>
 );
 
 export default toggleMap;
