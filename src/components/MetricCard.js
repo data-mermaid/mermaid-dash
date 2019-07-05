4369d7b84de5dd8c1faddb2cd147d6a734c16f08
Nick Hoang <nickhoang11@gmail.com>
2019-07-05 14:19:51 -0700

    Add metric cards components

---
+++ src/components/MetricCard.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+const cardStyle = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(1, 0),
+    textAlign: 'center'
+  },
+  title: {
+    borderTop: '1px solid',
+    padding: theme.spacing(1, 1)
+  },
+  body: {
+    color: '#5080AD',
+    padding: theme.spacing(2, 2)
+  }
+}));
+
+const metricCard = props => {
+  const classes = cardStyle();
+  const { title, count } = props.card;
+  const contentItem = count ? <Box className={classes.body}>{count}</Box> : null;
+  const titleItem = title ? <Box className={classes.title}>{title}</Box> : null;
+
+  return (
+    <Paper className={classes.root}>
+      <Typography variant="h2">{contentItem}</Typography>
+      <Typography variant="body1" component="div">
+        {titleItem}
+      </Typography>
+    </Paper>
+  );
+};
+
+export default metricCard;
---
+++ src/components/MetricCards.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import summary from '../apis/summary';
+
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import MetricCard from './MetricCard';
+
+const metricStyle = theme => ({
+  root: {
+    padding: theme.spacing(2, 0)
+  }
+});
+
+class MetricCards extends Component {
+  state = {
+    metrics: [
+      { title: 'Countries', count: 10 },
+      { title: 'Projects', count: 2 },
+      { title: 'Users', count: 20 },
+      { title: 'Sites', count: null },
+      { title: 'Transects', count: 5 },
+      { title: 'Avg Coral Coverage', count: 12 }
+    ]
+  };
+
+  async componentDidMount() {
+    const {
+      data: { features }
+    } = await summary.get('/sites');
+
+    const metrics = [...this.state.metrics];
+    metrics[3].count = features.length;
+    this.setState({ metrics });
+  }
+
+  render() {
+    const { classes } = this.props;
+    const cardList = this.state.metrics.map((card, index) => {
+      return (
+        <Grid item xs={4} key={index}>
+          <MetricCard card={card} />
+        </Grid>
+      );
+    });
+
+    return (
+      <Grid container spacing={2} className={classes.root}>
+        {cardList}
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(metricStyle)(MetricCards);
