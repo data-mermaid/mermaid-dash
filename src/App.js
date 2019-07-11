37f687be1ca973c240a013bd89dcf7332de82cc9
Nick Hoang <nickhoang11@gmail.com>
2019-07-11 10:10:33 -0700

    Centralized api calls in app js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import summary from '../src/apis/summary';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 import Header from './components/Header';
@@ -8,11 +9,36 @@ import MapCanvas from './components/MapCanvas';
 
 class App extends Component {
   state = {
-    showFullMap: true
+    showFullMap: true,
+    sites: [],
+    selectSite: null,
+    metrics: [
+      { title: 'Countries', count: 10 },
+      { title: 'Projects', count: 2 },
+      { title: 'Users', count: 20 },
+      { title: 'Sites', count: null },
+      { title: 'Transects', count: 5 },
+      { title: 'Avg Coral Coverage', count: 12 }
+    ]
   };
+
+  async componentDidMount() {
+    const {
+      data: { features: sites }
+    } = await summary.get('/sites');
+    const metrics = [...this.state.metrics];
+    metrics[3].count = sites.length;
+
+    this.setState({ sites });
+    this.setState({ metrics });
+  }
 
   toggle = () => {
     this.setState({ showFullMap: !this.state.showFullMap });
+  };
+
+  siteSelectHandler = selectedOption => {
+    this.setState({ selectSite: selectedOption });
   };
 
   render() {
@@ -20,7 +46,13 @@ class App extends Component {
       <BrowserRouter>
         <Header toggle={this.toggle} />
         <MapCanvas />
-        <DashBoard showFullMap={this.state.showFullMap} />
+        <DashBoard
+          sites={this.state.sites}
+          siteSelectHandler={this.siteSelectHandler}
+          selectSite={this.state.selectSite}
+          showFullMap={this.state.showFullMap}
+          metrics={this.state.metrics}
+        />
       </BrowserRouter>
     );
   }
