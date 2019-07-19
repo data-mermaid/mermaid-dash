e26adf41acee9c7393e9219fc99896f266c28c52
Nick Hoang <nickhoang11@gmail.com>
2019-07-19 09:39:33 -0700

    Use flexbox to arrange menu item, remove grid layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,19 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
-import ToggleMap from './ToggleMap';
+import CheckBoxFullMap from './CheckBoxFullMap';
 
 const useStyles = makeStyles(theme => ({
-  menuItem: {
-    textAlign: 'center'
+  menuProperty: {
+    width: '100vw'
   },
-  color: {
+  appBarProperty: {
     background: '#2C3742'
+  },
+  checkBoxProperty: {
+    paddingTop: '8px'
   }
 }));
 
@@ -19,30 +22,30 @@ const Header = ({ toggle }) => {
   const classes = useStyles();
 
   return (
-    <AppBar position="static" className={classes.color}>
+    <AppBar position="static" className={classes.appBarProperty}>
       <Toolbar>
-        <Grid container>
-          <Grid item xs={7}>
-            <Typography variant="h4">MERMAID</Typography>
-          </Grid>
-          <Grid container justify="flex-end" alignItems="flex-start" item xs={5}>
-            <Grid item xs={2} className={classes.menuItem}>
+        <div className={classes.menuProperty}>
+          <Box display="flex">
+            <Box p={1} flexGrow={1}>
+              <Typography variant="h4">MERMAID</Typography>
+            </Box>
+            <Box p={2}>
               <Typography variant="overline">COLLECT</Typography>
-            </Grid>
-            <Grid item xs={3} className={classes.menuItem}>
-              <Typography variant="overline">ABOUT THESE DATA</Typography>
-            </Grid>
-            <Grid item xs={2} className={classes.menuItem}>
+            </Box>
+            <Box p={2}>
+              <Typography variant="overline">ABOUT THIS DATA</Typography>
+            </Box>
+            <Box p={2}>
               <Typography variant="overline">PRIVACY</Typography>
-            </Grid>
-            <Grid item xs={2} className={classes.menuItem}>
+            </Box>
+            <Box p={2}>
               <Typography variant="overline">CONTACT</Typography>
-            </Grid>
-            <Grid item xs={3} className={classes.menuItem}>
-              <ToggleMap toggle={toggle}>Show Full Map</ToggleMap>
-            </Grid>
-          </Grid>
-        </Grid>
+            </Box>
+            <Box className={classes.checkBoxProperty}>
+              <CheckBoxFullMap toggle={toggle} />
+            </Box>
+          </Box>
+        </div>
       </Toolbar>
     </AppBar>
   );
