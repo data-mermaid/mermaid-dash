91738dc69786f92f40eb7f14595d060f39c8bf9d
Nick Hoang <nickhoang11@gmail.com>
2019-07-23 15:48:32 -0700

    Add extra dummy data for piechart, and some adjustment to data for others

--- src/sample_data/sampleSummaryStatistic.js
+++ src/sample_data/sampleSummaryStatistic.js
@@ -1,11 +1,12 @@
 const Samples = {
   summary: {
     title: 'Summary Statistics',
+    subTitle: 'Data is more powerful when shared',
     body:
       'Mermaid believes in coral reef conservation through collaboration. We use cutting edge technology to visualize, analyse and share data about coral reefs in near real time, and for free.',
     type: 'text'
   },
-  chartData: {
+  barChartData: {
     title: 'Live Coral Cover',
     body: [
       { hardCorals: 1, sites: 50, label: 50 },
@@ -29,7 +30,68 @@ const Samples = {
       { hardCorals: 19, sites: 20, label: 20 },
       { hardCorals: 20, sites: 5, label: 5 }
     ],
-    type: 'chart'
+    legend: {},
+    type: 'barChart'
+  },
+  benthicPieChartData: {
+    title: 'Benthic Condition, % Cover',
+    body: [
+      { x: 'Bare substrate', y: 15 },
+      { x: 'Crustose coralline algae', y: 21 },
+      { x: 'Cyanobacteria', y: 14 },
+      { x: 'Hard coral', y: 10 },
+      { x: 'Macroalgae', y: 5 },
+      { x: 'Other invertebrates', y: 9 },
+      { x: 'Rubble', y: 6 },
+      { x: 'Sand', y: 3 },
+      { x: 'Soft coral', y: 4 },
+      { x: 'Turf algae', y: 3 }
+    ],
+    dataPolicy: 'Public Summary',
+    sampleUnits: 21,
+    legend: {
+      title: 'Benthic category',
+      data: [
+        { name: 'Bare substrate' },
+        { name: 'Crustose coralline algae' },
+        { name: 'Cyanobacteria' },
+        { name: 'Hard coral' },
+        { name: 'Macroalgae' },
+        { name: 'Other invertebrates' },
+        { name: 'Rubble' },
+        { name: 'Sand' },
+        { name: 'Soft coral' },
+        { name: 'Turf algae' }
+      ]
+    },
+    type: 'pieChart'
+  },
+  fishBeltPieChartData: {
+    title: 'Fish Belt',
+    body: [
+      { x: 'Herbivore-detritivore', y: 6 },
+      { x: 'Herbivore-macroalgae', y: 8 },
+      { x: 'Invertivore-mobile', y: 12 },
+      { x: 'Invertivore-sessile', y: 10 },
+      { x: 'Omnivore', y: 20 },
+      { x: 'Piscivore', y: 14 },
+      { x: 'Planktivore', y: 30 }
+    ],
+    dataPolicy: 'Public Summary',
+    sampleUnits: 10,
+    legend: {
+      title: 'Tropic group',
+      data: [
+        { name: 'Herbivore-detritivore' },
+        { name: 'Herbivore-macroalgae' },
+        { name: 'Invertivore-mobile' },
+        { name: 'Invertivore-sessile' },
+        { name: 'Omnivore' },
+        { name: 'Piscivore' },
+        { name: 'Planktivore' }
+      ]
+    },
+    type: 'pieChart'
   }
 };
 
