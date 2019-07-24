237eba5dd3774f069899e591ad6c8446958e8053
Nick Hoang <nickhoang11@gmail.com>
2019-07-24 15:19:17 -0700

    Change to map layer with label

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -76,10 +76,10 @@ class LeafletMap extends Component {
     });
 
     L.tileLayer(
-      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
       {
         attribution:
-          'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'
       }
     ).addTo(this.map);
 
