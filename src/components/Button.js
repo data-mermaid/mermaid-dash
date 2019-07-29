7011fc7de21de769c1bf77156effd2758440aa78
Nick Hoang <nickhoang11@gmail.com>
2019-07-29 10:07:10 -0700

    Add button style for full zoom. Update some property for button component.

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,13 +5,16 @@ export const ButtonStyle = styled('button')`
   transition: ${props => props.theme.transition};
   color: ${props => props.theme.fg};
   ${props =>
-    props.buttonBorder &&
-    css`
-      border: 1px solid ${props => props.theme.fg};
-    `}
+    props.buttonBorder
+      ? css`
+          border: 5px solid ${props => props.theme.fg};
+        `
+      : css`
+          border: none;
+        `}
   background: ${props => props.theme.bg};
   font-size: 1em;
-  border-radius: 5px;
+  border-radius: 4px;
   position: ${props => props.theme.position};
   padding: ${props => props.theme.padding};
   width: ${props => props.theme.width};
@@ -22,5 +25,8 @@ export const ButtonStyle = styled('button')`
       props.setHover ? '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)' : null};
     transform: ${props => props.theme.transform};
     transition: ${props => props.theme.transition};
+  }
+  &:focus {
+    outline: none;
   }
 `;
--- src/components/theme.js
+++ src/components/theme.js
@@ -1,6 +1,5 @@
 export const backButtonTheme = {
   fg: '#2C3742',
-  border: '0px',
   position: 'fixed',
   width: '70px',
   padding: '5px',
@@ -13,9 +12,16 @@ export const backButtonTheme = {
 export const cardButtonTheme = {
   fg: 'white',
   bg: '#468DAE',
-  border: '1px',
   width: '150px',
   padding: '1px',
   shadow:
     '0px 1px 3px 0px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)'
+};
+
+export const fullZoomTheme = {
+  fg: 'white',
+  bg: 'black',
+  position: 'fixed',
+  width: '27px',
+  padding: '2px'
 };
