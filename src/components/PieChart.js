625cf0ab5ea81a0a70c6f4e922eaf63a3ab38e04
Nick Hoang <nickhoang11@gmail.com>
2019-07-31 10:40:37 -0700

    Put label in the center of pie chart, and made a few style changes.

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -1,91 +1,130 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { VictoryPie, VictoryLegend } from 'victory';
+import styled from 'styled-components';
 
 // import PropTypes from 'prop-types';
+const LabelContainer = styled('div')`
+  position: relative;
+  top: 190px;
+  left: 100px;
+  width: 180px;
+`;
 
-const PieChart = ({ chartContent, chartLegend }) => (
-  <svg width="100%" height={450}>
-    <VictoryLegend
-      standalone={false}
-      colorScale={[
-        '#4B3991',
-        '#8B0033',
-        '#C8283D',
-        '#EE5634',
-        '#FB9E4F',
-        '#FDDA79',
-        '#E0F686',
-        '#9ED893',
-        '#57B894',
-        '#2A74AF'
-      ]}
-      x={500}
-      y={40}
-      gutter={20}
-      title={chartLegend.title}
-      centerTitle
-      style={{ border: { stroke: 'black' } }}
-      data={chartLegend.data}
-    />
-    <VictoryPie
-      standalone={false}
-      innerRadius={100}
-      width={500}
-      padding={{
-        right: 10,
-        bottom: 20,
-        top: 50
-      }}
-      labels={() => null}
-      colorScale={[
-        '#4B3991',
-        '#8B0033',
-        '#C8283D',
-        '#EE5634',
-        '#FB9E4F',
-        '#FDDA79',
-        '#E0F686',
-        '#9ED893',
-        '#57B894',
-        '#2A74AF'
-      ]}
-      data={chartContent}
-      events={[
-        {
-          target: 'data',
-          eventHandlers: {
-            onMouseOver: () => {
-              return [
-                {
-                  target: 'data',
-                  mutation: () => ({ style: { fill: 'black', opacity: 0.5, cursor: 'pointer' } })
+const LabelProperty = styled('p')`
+  text-align: center;
+  font-size: 16px;
+`;
+
+const NoLabelProperty = styled('p')`
+  visibility: hidden;
+  font-size: 16px;
+`;
+
+const defaultColorScale = [
+  '#4B3991',
+  '#8B0033',
+  '#C8283D',
+  '#EE5634',
+  '#FB9E4F',
+  '#FDDA79',
+  '#E0F686',
+  '#9ED893',
+  '#57B894',
+  '#2A74AF'
+];
+
+const PieChart = ({ chartContent, chartLegend }) => {
+  const [centerLabel, setCenterLabel] = useState({ number: null, label: null });
+  const labelControl =
+    centerLabel.number || centerLabel.label ? (
+      <LabelContainer>
+        <LabelProperty>{centerLabel.label}</LabelProperty>
+        <LabelProperty>{centerLabel.number}</LabelProperty>
+      </LabelContainer>
+    ) : (
+      <LabelContainer>
+        <NoLabelProperty>no label</NoLabelProperty>
+        <NoLabelProperty>no label</NoLabelProperty>
+      </LabelContainer>
+    );
+
+  return (
+    <>
+      {labelControl}
+      <svg width="100%" height={360}>
+        <VictoryLegend
+          standalone={false}
+          colorScale={defaultColorScale}
+          x={430}
+          title={chartLegend.title}
+          centerTitle
+          style={{
+            title: { fontSize: 20 }
+          }}
+          data={chartLegend.data}
+        />
+        <VictoryPie
+          standalone={false}
+          innerRadius={120}
+          height={300}
+          width={430}
+          padding={{
+            right: 80,
+            left: 40
+          }}
+          labels={() => null}
+          colorScale={defaultColorScale}
+          data={chartContent}
+          events={[
+            {
+              target: 'data',
+              eventHandlers: {
+                onMouseOver: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: data => {
+                        const {
+                          datum: { x: label, y: number }
+                        } = data;
+                        setCenterLabel({
+                          number,
+                          label
+                        });
+                      }
+                    },
+                    {
+                      target: 'data',
+                      mutation: () => ({
+                        style: { fill: 'smoke', opacity: 0.6, cursor: 'pointer' }
+                      })
+                    }
+                  ];
                 },
-                {
-                  target: 'labels',
-                  mutation: props => {
-                    const labelCLicked = props.datum.x + '\n' + props.datum.y;
-                    return { active: true, text: labelCLicked };
-                  }
+                onMouseOut: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: data => {
+                        setCenterLabel({
+                          number: null,
+                          label: null
+                        });
+                      }
+                    },
+                    {
+                      target: 'data',
+                      mutation: () => {}
+                    }
+                  ];
                 }
-              ];
-            },
-            onMouseOut: () => {
-              return [
-                {
-                  target: 'data',
-                  mutation: () => {}
-                },
-                {
-                  target: 'labels',
-                  mutation: () => ({ active: false })
-                }
-              ];
+              }
             }
-          }
-        }
-      ]}
-    />
-  </svg>
-);
+          ]}
+        />
+      </svg>
+    </>
+  );
+};
 
 export default PieChart;
