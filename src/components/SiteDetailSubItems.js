a36a7a3ab37d5f6c84b6be92e6ef15afc2181a3c
Nick Hoang <nickhoang11@gmail.com>
2019-09-06 14:48:27 -0700

    Rename variables

--- src/components/SiteDetailSubItems.js
+++ src/components/SiteDetailSubItems.js
@@ -18,9 +18,9 @@ const subItemStyles = makeStyles(theme => ({
 
 const SiteDetailSubItems = ({ loadedSiteProperties }) => {
   const classes = subItemStyles();
-  const dateMin = loadedSiteProperties.date_min.substring(0, 4);
-  const dateMax = loadedSiteProperties.date_max.substring(0, 4);
-  const siteYear = dateMax === dateMin ? dateMax : `${dateMin} - ${dateMax}`;
+  const startDate = loadedSiteProperties.date_min.substring(0, 4);
+  const endDate = loadedSiteProperties.date_max.substring(0, 4);
+  const siteYear = endDate === startDate ? endDate : `${startDate} - ${endDate}`;
 
   const contactButton = (
     <ThemeProvider theme={theme.cardButton}>
