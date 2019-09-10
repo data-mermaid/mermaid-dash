cd6038b7b3f3c2f5e987df5441e56da31a6b1071
Nick Hoang <nickhoang11@gmail.com>
2019-09-09 19:26:09 -0700

    Put custom styled scrollbar style to styled compoennt

--- src/components/DashBoard.js
+++ src/components/DashBoard.js
@@ -28,6 +28,22 @@ import PropTypes from 'prop-types';
 const RootGrid = styled(Grid)`
   overflow-y: scroll;
   height: calc(100vh - 49px);
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    border-radius: 10px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar {
+    width: 12px;
+    background-color: #f5f5f5;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+    background-color: #2d3641;
+  }
 `;
 
 const gridStyleProperties = makeStyles(theme => ({
