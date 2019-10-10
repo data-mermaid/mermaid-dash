1b5b71f042fe8ecdd63fc0a4512657e70412c235
Nick Hoang <nickhoang11@gmail.com>
2019-10-10 16:19:30 -0700

    Assign a fixed color for each benthic attribute. This also serves as color constants to switch between different color scales

---
+++ src/constants/attribute-colors.js
@@ -0,0 +1,24 @@
+export const attributeColors = [
+  { name: 'Hard coral', color: '#498FC9' },
+  { name: 'Soft coral', color: '#9CE5FA' },
+  { name: 'Macroalgae', color: '#B6B400' },
+  { name: 'Turf algae', color: '#DEF4AC' },
+  { name: 'Cyanobacteria', color: '#870E00' },
+  { name: 'Crustose coralline algae', color: '#FBD7D5' },
+  { name: 'Rubble', color: '#FAFBB2' },
+  { name: 'Bare substrate', color: '#F2F3F3' },
+  { name: 'Sand', color: '#C2B280' },
+  { name: 'Other invertebrates', color: '#4E4E4E' },
+  { name: 'Seagrass', color: '#494949' }
+];
+
+export const privateColorScale = [
+  '#DCDCDC',
+  '#D3D3D3',
+  '#C0C0C0',
+  '#A9A9A9',
+  '#808080',
+  '#696969',
+  '#778899',
+  '#708090'
+];
