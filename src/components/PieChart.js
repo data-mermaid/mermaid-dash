a6c5fcc993b555a4bead8c242f09b6fc5e9c83b0
Nick Hoang <nickhoang11@gmail.com>
2019-10-22 12:07:45 -0700

    Redo the private text for private protocol.

--- src/components/PieChart.js
+++ src/components/PieChart.js
@@ -14,6 +14,18 @@ const ChartWrapper = styled('div')`
   filter: ${props => props.policy && 'blur(0.8rem)'};
 `;
 
+const PrivateTextWrapper = styled('div')`
+  position: relative;
+  height: 0;
+  width: 60%;
+  top: ${props => (props.mediaMin600_Max960 ? '170px' : '180px')};
+  left: ${props => (props.mediaMin960_Max1280 || props.mediaMax600 ? '100px' : '150px')};
+  z-index: 1;
+  user-select: none;
+  text-align: center;
+  font-size: large;
+`;
+
 const SvgWrapper = styled('svg')`
   width: 360px;
   ${props =>
@@ -25,11 +37,13 @@ const SvgWrapper = styled('svg')`
 
 const PieChart = ({ protocolName, chartContent, setToPrivate, privateLabel }) => {
   const mediaMin600 = useMediaQuery('(min-width:600px)');
+  const mediaMax600 = useMediaQuery('(max-width:600px)');
   const mediaMax960 = useMediaQuery('(max-width:960px');
   const mediaMin960 = useMediaQuery('(min-width:960px)');
   const mediaMin1281 = useMediaQuery('(min-width:1281px)');
   const mediaMax1280 = useMediaQuery('(max-width:1280px)');
   const mediaMin600_Max960 = mediaMax960 && mediaMin600;
+  const mediaMin960_Max1280 = mediaMax1280 && mediaMin960;
 
   const benthicAttributeCollection = chartContent.map(({ x }) => x);
   const filteredAttributeCollection = attributeColors.filter(({ name }) =>
@@ -47,91 +61,104 @@ const PieChart = ({ protocolName, chartContent, setToPrivate, privateLabel }) =>
     .filter(({ y }) => y > 0)
     .map(({ x }) => ({ name: x, symbol: { type: 'square' } }));
 
-  return (
-    <ChartWrapper
-      mediaMax1280={mediaMax1280}
+  const privateLabelText = setToPrivate && (
+    <PrivateTextWrapper
+      mediaMax600={mediaMax600}
       mediaMin600_Max960={mediaMin600_Max960}
-      policy={setToPrivate}
+      mediaMin960_Max1280={mediaMin960_Max1280}
     >
-      <VictoryPie
-        innerRadius={mediaMax1280 ? 90 : 110}
-        height={mediaMax1280 ? 300 : 400}
-        width={mediaMax1280 ? 400 : 450}
-        padding={mediaMax1280 ? 30 : 50}
-        labels={val => {
-          return val.x + '\n' + val.y.toFixed(1) + '%';
-        }}
-        labelComponent={
-          <VictoryTooltip
-            x={mediaMax1280 ? 200 : 220}
-            y={mediaMax1280 ? 190 : 230}
-            orientation="top"
-            pointerLength={0}
-            flyoutStyle={{
-              stroke: 'transparent',
-              fill: 'transparent'
-            }}
-          />
-        }
-        colorScale={setToPrivate ? privateColorScale : benthicsColorScale}
-        data={setToPrivate ? chartContent : contentData}
-        events={[
-          {
-            target: 'data',
-            eventHandlers: {
-              onMouseOver: () => {
-                return [
-                  {
-                    target: 'data',
-                    mutation: () => ({
-                      style: {
-                        fill: 'smoke',
-                        opacity: 0.3,
-                        transition: '0.25s ease-in-out'
-                      }
-                    })
-                  },
-                  {
-                    target: 'labels',
-                    mutation: () => ({ active: true, fontSize: 20 })
-                  }
-                ];
-              },
-              onMouseOut: () => {
-                return [
-                  {
-                    target: 'data',
-                    mutation: () => {}
-                  },
-                  {
-                    target: 'labels',
-                    mutation: () => ({ active: false })
-                  }
-                ];
+      {privateLabel}
+    </PrivateTextWrapper>
+  );
+
+  return (
+    <>
+      {privateLabelText}
+      <ChartWrapper
+        mediaMax1280={mediaMax1280}
+        mediaMin600_Max960={mediaMin600_Max960}
+        policy={setToPrivate}
+      >
+        <VictoryPie
+          innerRadius={mediaMax1280 ? 90 : 110}
+          height={mediaMax1280 ? 300 : 400}
+          width={mediaMax1280 ? 400 : 450}
+          padding={mediaMax1280 ? 30 : 50}
+          labels={val => {
+            return val.x + '\n' + val.y.toFixed(1) + '%';
+          }}
+          labelComponent={
+            <VictoryTooltip
+              x={mediaMax1280 ? 200 : 220}
+              y={mediaMax1280 ? 190 : 230}
+              orientation="top"
+              pointerLength={0}
+              flyoutStyle={{
+                stroke: 'transparent',
+                fill: 'transparent'
+              }}
+            />
+          }
+          colorScale={setToPrivate ? privateColorScale : benthicsColorScale}
+          data={setToPrivate ? chartContent : contentData}
+          events={[
+            {
+              target: 'data',
+              eventHandlers: {
+                onMouseOver: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: () => ({
+                        style: {
+                          fill: 'smoke',
+                          opacity: 0.3,
+                          transition: '0.25s ease-in-out'
+                        }
+                      })
+                    },
+                    {
+                      target: 'labels',
+                      mutation: () => ({ active: true, fontSize: 20 })
+                    }
+                  ];
+                },
+                onMouseOut: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: () => {}
+                    },
+                    {
+                      target: 'labels',
+                      mutation: () => ({ active: false })
+                    }
+                  ];
+                }
               }
             }
-          }
-        ]}
-      />
-      <SvgWrapper setHeight={mediaMin600_Max960 || !mediaMax1280}>
-        <VictoryLegend
-          standalone={false}
-          colorScale={setToPrivate ? privateColorScale : benthicsColorScale}
-          orientation="horizontal"
-          height={mediaMin1281 ? 500 : mediaMin600_Max960 ? 310 : 140}
-          itemsPerRow={
-            mediaMin1281 || mediaMin600_Max960 || legendData.length <= 4 ? 1 : mediaMin960 ? 3 : 2
-          }
-          style={{
-            labels: {
-              fontSize: mediaMin1281 ? 15 : mediaMin600_Max960 ? 14 : 11,
-              fontFamily: 'Arial'
-            }
-          }}
-          data={legendData}
+          ]}
         />
-      </SvgWrapper>
-    </ChartWrapper>
+        <SvgWrapper setHeight={mediaMin600_Max960 || !mediaMax1280}>
+          <VictoryLegend
+            standalone={false}
+            colorScale={setToPrivate ? privateColorScale : benthicsColorScale}
+            orientation="horizontal"
+            height={mediaMin1281 ? 500 : mediaMin600_Max960 ? 310 : 140}
+            itemsPerRow={
+              mediaMin1281 || mediaMin600_Max960 || legendData.length <= 4 ? 1 : mediaMin960 ? 3 : 2
+            }
+            style={{
+              labels: {
+                fontSize: mediaMin1281 ? 15 : mediaMin600_Max960 ? 14 : 11,
+                fontFamily: 'Arial'
+              }
+            }}
+            data={legendData}
+          />
+        </SvgWrapper>
+      </ChartWrapper>
+    </>
   );
 };
 
