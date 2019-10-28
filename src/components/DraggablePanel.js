721cea7652e8836e1772e2f932bf8a4bed9bb92b
Nick Hoang <nickhoang11@gmail.com>
2019-10-28 11:52:48 -0700

    Testing on responsive height for the draggable component

--- src/components/DraggablePanel.js
+++ src/components/DraggablePanel.js
@@ -2,18 +2,17 @@ import React, { useState } from 'react';
 import styled from 'styled-components/macro';
 import Draggable, { DraggableCore } from 'react-draggable';
 
+console.log(window.innerHeight);
 const Container = styled('div')`
   position: fixed;
   width: 100%;
-  /* bottom: 0; */
-  height: calc(100vh - 100px);
+  height: 1000px;
   border: 2px solid red;
   overflow-y: hidden;
   z-index: 1000;
   justify-content: center;
   display: flex;
   background: rgba(50, 50, 50, 0.5);
-  /* top: ${props => (props.open ? '80vh' : 0)}; */
   top: 80vh;
   .handle {
     width: 30px;
@@ -28,21 +27,21 @@ const Container = styled('div')`
 
 const DraggablePanel = () => {
   const [open, setOpen] = useState(false);
-  const [triggered, setTriggered] = useState(false);
 
-  const bounds = {
-    open: { top: -600, left: 0, right: 0, bottom: 200 },
-    closed: { top: 0, left: 0, right: 0, bottom: 600 }
-  };
-
-  const handleStop = (e, { y }) => {
+  const handleStop = (evt, val) => {
     console.log('handle stop');
-    console.log('y from stop ', y);
-    if (!open && y < -25) {
+    console.log('e ', evt);
+    console.log('val from stop ', val);
+    if (!open && val.y < -25) {
       setOpen(true);
-    } else if (open && y > -684) {
+    } else if (open && val.y > -684) {
       setOpen(false);
     }
+  };
+
+  const calBounds = windowHeight => {
+    // console.log('calbounds ', (windowHeight * -800) / 980);
+    return (windowHeight * -800) / 1180;
   };
 
   return (
@@ -50,7 +49,7 @@ const DraggablePanel = () => {
       axis="y"
       handle=".handle"
       onStop={handleStop}
-      position={open ? { x: 0, y: -709 } : { x: 0, y: 0 }}
+      position={open ? { x: 0, y: calBounds(window.innerHeight) } : { x: 0, y: 0 }}
     >
       <Container open={open}>
         <div className="handle" />
