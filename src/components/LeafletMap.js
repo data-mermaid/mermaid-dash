f2e6db8e0ddccd8e469ddf0e78920f60dc471eb7
Nick Hoang <nickhoang11@gmail.com>
2019-10-31 09:36:01 -0700

    Testing margin top

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -19,9 +19,10 @@ L.Icon.Default.mergeOptions({
 const MapContainer = styled.div`
   height: 100%;
   width: 100%;
-  position: fixed;
-  top: 49px;
+  /* position: fixed;
+  top: 49px; */
   /* padding-top: 49px; */
+  margin-top: 49px;
   padding-right: ${props => (props.sidePanelOpen ? '650px' : '0px')};
 
   @media (min-width: 0px) {
