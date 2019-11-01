23113e398a0105a0fda2c0e7089a45919675dd0c
Nick Hoang <nickhoang11@gmail.com>
2019-11-01 16:38:56 -0700

    Remove useMediaQuery hook, replace with breakpoint and media css useMediaQuery hook isn't compatible for safari and iphone, etc...

--- src/components/MetricCard.js
+++ src/components/MetricCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
 import styled, { css } from 'styled-components/macro';
 
 import CircularProgress from '@material-ui/core/CircularProgress';
@@ -10,25 +9,35 @@ import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 
 const TitleBoxStyle = styled.div`
-  border-top: ${props => (props.mediaMax960 ? '1px solid' : '0')};
+  border-top: 0;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 11px;
   ${props =>
-    (props.mediaMax960 || props.bottomPanelOpen) &&
+    props.bottomPanelOpen &&
     css`
       height: 58px;
       font-weight: bold;
       font-size: 18px;
     `}
+
+  @media (min-width: 960px) {
+    border-top: 1px solid;
+    height: 58px;
+    font-weight: bold;
+    font-size: 18px;
+  }
 `;
 
 const ContentBoxStyle = styled.div`
   color: #5080ad;
-  padding: ${props =>
-    props.mediaMax960 ? '30px 0px 30px 0' : props.bottomPanelOpen && '30px 0px 0px 0'};
-  font-size: ${props => (props.mediaMax960 || props.bottomPanelOpen ? '55px' : '32px')};
+  padding: ${props => props.bottomPanelOpen && '30px 0px 0px 0'};
+  font-size: ${props => (props.bottomPanelOpen ? '55px' : '32px')};
+  @media (min-width: 960px) {
+    padding: 30px 0px 30px 0;
+    font-size: 55px;
+  }
 `;
 
 const WrapperMetricCard = styled(Paper)`
@@ -37,36 +46,32 @@ const WrapperMetricCard = styled(Paper)`
 `;
 
 const cardStyle = makeStyles(theme => ({
-  mobileProgress: {
-    margin: '7px'
-  },
   progress: {
-    margin: '39px'
+    margin: '39px',
+    width: '60px !important',
+    height: '60px !important',
+    [theme.breakpoints.down('sm')]: {
+      margin: '7px',
+      width: '31px !important',
+      height: '31px !important'
+    }
   }
 }));
 
 const MetricCard = ({ content: { title, count }, isLoading, bottomPanelOpen }) => {
   const classes = cardStyle();
-  const mediaMax960 = useMediaQuery('(min-width:960px');
 
   const countContent = title === 'Avg Coral Coverage' ? `${count}%` : count;
 
   const contentItem =
     count !== null && count >= 0 && !isLoading ? (
-      <ContentBoxStyle mediaMax960={mediaMax960} bottomPanelOpen={bottomPanelOpen}>
-        {countContent}
-      </ContentBoxStyle>
+      <ContentBoxStyle bottomPanelOpen={bottomPanelOpen}>{countContent}</ContentBoxStyle>
     ) : (
-      <CircularProgress
-        size={mediaMax960 ? 60 : 31}
-        className={mediaMax960 ? classes.progress : classes.mobileProgress}
-      />
+      <CircularProgress className={classes.progress} />
     );
 
   const titleItem = title && (
-    <TitleBoxStyle mediaMax960={mediaMax960} bottomPanelOpen={bottomPanelOpen}>
-      {title}
-    </TitleBoxStyle>
+    <TitleBoxStyle bottomPanelOpen={bottomPanelOpen}>{title}</TitleBoxStyle>
   );
 
   return (
