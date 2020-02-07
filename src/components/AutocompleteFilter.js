baf7b56a65cb99b5814dc830177235e7b728e40b
Nick Hoang <nickhoang11@gmail.com>
2020-02-07 15:22:53 -0800

    Add autocomplete highlight to autocomplete

--- src/components/AutocompleteFilter.js
+++ src/components/AutocompleteFilter.js
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
+import parse from 'autosuggest-highlight/parse';
+import match from 'autosuggest-highlight/match';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
@@ -31,6 +33,20 @@ const AutocompleteFilter = ({ id, label, options, preFilledValues, addQueryStrin
         }}
         filterSelectedOptions
         renderInput={params => <TextField {...params} variant="outlined" label={label} fullWidth />}
+        renderOption={(option, { inputValue }) => {
+          const matches = match(option, inputValue);
+          const parts = parse(option, matches);
+
+          return (
+            <div>
+              {parts.map((part, index) => (
+                <span key={index} style={{ fontWeight: part.highlight ? 700 : 400 }}>
+                  {part.text}
+                </span>
+              ))}
+            </div>
+          );
+        }}
       />
     </div>
   );
