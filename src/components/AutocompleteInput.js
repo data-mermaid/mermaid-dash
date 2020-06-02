89e7ee72b58a2ec797c3a70187797ea4ec7af040
Nick Hoang <nickhoang11@gmail.com>
2020-06-02 12:06:56 -0700

    Fix change request for sort

--- src/components/AutocompleteInput.js
+++ src/components/AutocompleteInput.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import AutocompleteFilter from './AutocompleteFilter';
+import byCaseSensitive from '../lib/case-sensitive-sort-fix';
 
 const AutocompleteInput = ({ filterParams, addQueryStrings, filterChoices }) => {
-  const country_names = filterChoices.countries.map(country => country.name).sort();
-  const project_names = filterChoices.projects.map(project => project.name).sort();
-  const tag_names = filterChoices.tags.map(tag => tag.name).sort();
+  const country_names = filterChoices.countries.map(country => country.name).sort(byCaseSensitive);
+  const project_names = filterChoices.projects.map(project => project.name).sort(byCaseSensitive);
+  const tag_names = filterChoices.tags.map(tag => tag.name).sort(byCaseSensitive);
 
   const convertToName = (ids, filtered_options) => {
     const result = filtered_options.reduce((newArr, obj) => {
