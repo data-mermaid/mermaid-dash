86a89b668687490aa422832940b6c1748681108d
Nick Hoang <nickhoang11@gmail.com>
2020-06-04 16:50:14 -0700

    Y axis is now more reponsive

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -3,87 +3,97 @@ import { VictoryBar, VictoryChart, VictoryAxis, VictoryTooltip } from 'victory';
 
 import PropTypes from 'prop-types';
 
-const BarChart = ({ chartContent }) => (
-  <VictoryChart domainPadding={9}>
-    <VictoryAxis
-      label="% Hard Coral Cover"
-      tickValues={[0.05, 0.25, 0.5, 0.75, 1]}
-      tickFormat={['5', '25', '50', '75', '100']}
-      style={{
-        tickLabels: { fontSize: 10, padding: 5 }
-      }}
-    />
+const calculateYs = content => {
+  return content.reduce((total, content) => {
+    return total + content.y;
+  }, 0);
+};
 
-    <VictoryAxis
-      dependentAxis
-      label="Number of Sites"
-      tickValues={[0, 5, 10, 21]}
-      tickFormat={['0', '5', '10', '> 20']}
-      style={{
-        tickLabels: { fontSize: 10, padding: 5 }
-      }}
-    />
-    <VictoryBar
-      data={chartContent}
-      barWidth={17}
-      style={{ data: { fill: '#1E90FF' } }}
-      animate={{
-        duration: 1000,
-        onLoad: { duration: 500 },
-        onEnter: { duration: 400, before: () => ({ y: 0 }) }
-      }}
-      labelComponent={
-        <VictoryTooltip
-          cornerRadius={10}
-          pointerLength={0}
-          flyoutStyle={{
-            stroke: 'cadetblue',
-            strokeWidth: 2,
-            fill: 'skyblue',
-            opacity: 0.75
-          }}
+const BarChart = ({ chartContent }) => {
+  const nonZeroContent = calculateYs(chartContent);
+
+  if (chartContent)
+    return (
+      <VictoryChart domainPadding={9}>
+        <VictoryAxis
+          label="% Hard Coral Cover"
+          tickValues={[0.05, 0.25, 0.5, 0.75, 1]}
+          tickFormat={['5', '25', '50', '75', '100']}
           style={{
-            padding: 8,
-            fontSize: 12
+            tickLabels: { fontSize: 10, padding: 5 }
           }}
         />
-      }
-      events={[
-        {
-          target: 'data',
-          eventHandlers: {
-            onMouseOver: () => {
-              return [
-                {
-                  target: 'data',
-                  mutation: () => ({ style: { fill: 'tomato', width: 30 } })
-                },
-                {
-                  target: 'labels',
-                  mutation: () => ({ active: true })
-                }
-              ];
-            },
-            onMouseOut: () => {
-              return [
-                {
-                  target: 'data',
-                  mutation: () => {}
-                },
-                {
-                  target: 'labels',
-                  mutation: () => ({ active: false })
-                }
-              ];
-            }
+
+        <VictoryAxis
+          dependentAxis
+          label="Number of Sites"
+          tickValues={nonZeroContent > 0 ? [] : ['0', '20', '40', '60']}
+          style={{
+            tickLabels: { fontSize: 10, padding: 5 }
+          }}
+        />
+        <VictoryBar
+          data={chartContent}
+          barWidth={17}
+          style={{ data: { fill: '#1E90FF' } }}
+          animate={{
+            duration: 1000,
+            onLoad: { duration: 500 },
+            onEnter: { duration: 400, before: () => ({ y: 0 }) }
+          }}
+          labelComponent={
+            <VictoryTooltip
+              cornerRadius={10}
+              pointerLength={0}
+              flyoutStyle={{
+                stroke: 'cadetblue',
+                strokeWidth: 2,
+                fill: 'skyblue',
+                opacity: 0.75
+              }}
+              style={{
+                padding: 8,
+                fontSize: 12
+              }}
+            />
           }
-        }
-      ]}
-      x="x"
-      y="y"
-    />
-  </VictoryChart>
-);
+          events={[
+            {
+              target: 'data',
+              eventHandlers: {
+                onMouseOver: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: () => ({ style: { fill: 'tomato', width: 30 } })
+                    },
+                    {
+                      target: 'labels',
+                      mutation: () => ({ active: true })
+                    }
+                  ];
+                },
+                onMouseOut: () => {
+                  return [
+                    {
+                      target: 'data',
+                      mutation: () => {}
+                    },
+                    {
+                      target: 'labels',
+                      mutation: () => ({ active: false })
+                    }
+                  ];
+                }
+              }
+            }
+          ]}
+          x="x"
+          y="y"
+        />
+      </VictoryChart>
+    );
+};
 
 BarChart.propTypes = {
   chartContent: PropTypes.array
