471a18440c63623ea1b2566fb79fb205500a6e0d
tienhoah <nickhoang11@gmail.com>
2022-04-08 14:41:31 -0700

    Add Date Picker, and remove Date Input.

--- src/components/DatePickerInputs.js
+++ src/components/DatePickerInputs.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components/macro';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, DatePicker } from '@material-ui/pickers';
 import IconButton from '@material-ui/core/IconButton';
 import ClearIcon from '@material-ui/icons/Clear';
 
+const DatePickerWrapper = styled(DatePicker)`
+  width: 100%;
+`;
 const formatDateQuery = date => {
   const year = date?.getFullYear();
   const month = date?.getMonth() + 1;
@@ -12,10 +16,19 @@ const formatDateQuery = date => {
   return year || month || day ? `${year}-${month}-${day}` : null;
 };
 
-const DatePickerInputs = ({ filterParams, addQueryStrings }) => {
+const DatePickerInputs = ({ filterParams, addQueryStrings, setDateValidation }) => {
   const [startDate, setStateDate] = useState(filterParams.sample_date_after || null);
   const [endDate, setEndDate] = useState(filterParams.sample_date_before || null);
 
+  useEffect(() => {
+    if (startDate && endDate) {
+      const startDateTime = new Date(startDate).getTime();
+      const endDateTime = new Date(endDate).getTime();
+      setDateValidation(startDateTime > endDateTime);
+    } else {
+      setDateValidation(false);
+    }
+  }, [endDate, setDateValidation, startDate]);
   function handleClrStartDate(e) {
     e.stopPropagation();
     setStateDate(null);
@@ -29,46 +42,49 @@ const DatePickerInputs = ({ filterParams, addQueryStrings }) => {
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <div>
-        <DatePicker
-          autoOk
-          format="MMM dd, yyyy"
-          id="start-date-picker"
-          label="Start Date"
-          value={startDate}
-          views={['year', 'month', 'date']}
-          onChange={date => {
-            const dateQuery = formatDateQuery(date);
-            console.log('on accept');
-            setStateDate(date);
-            addQueryStrings('sample_date_after', dateQuery);
-          }}
-        />
-        {startDate && (
-          <IconButton onClick={e => handleClrStartDate(e)}>
-            <ClearIcon />
-          </IconButton>
-        )}
-      </div>
-      <div>
-        <DatePicker
-          autoOk
-          format="MMM dd, yyyy"
-          id="end-date-picker"
-          label="End Date"
-          value={endDate}
-          onChange={date => {
-            const dateQuery = formatDateQuery(date);
-            setEndDate(date);
-            addQueryStrings('sample_date_before', dateQuery);
-          }}
-        />
-        {endDate && (
-          <IconButton onClick={e => handleClrEndDate(e)}>
-            <ClearIcon />
-          </IconButton>
-        )}
-      </div>
+      <DatePickerWrapper
+        autoOk
+        format="MMM dd, yyyy"
+        id="start-date-picker"
+        label="Start Date"
+        value={startDate}
+        views={['year', 'month', 'date']}
+        onChange={date => {
+          const dateQuery = formatDateQuery(date);
+          setStateDate(date);
+          addQueryStrings('sample_date_after', dateQuery);
+        }}
+        InputProps={
+          startDate && {
+            endAdornment: (
+              <IconButton onClick={e => handleClrStartDate(e)}>
+                <ClearIcon />
+              </IconButton>
+            )
+          }
+        }
+      />
+      <DatePickerWrapper
+        autoOk
+        format="MMM dd, yyyy"
+        id="end-date-picker"
+        label="End Date"
+        value={endDate}
+        onChange={date => {
+          const dateQuery = formatDateQuery(date);
+          setEndDate(date);
+          addQueryStrings('sample_date_before', dateQuery);
+        }}
+        InputProps={
+          endDate && {
+            endAdornment: (
+              <IconButton onClick={e => handleClrEndDate(e)}>
+                <ClearIcon />
+              </IconButton>
+            )
+          }
+        }
+      />
     </MuiPickersUtilsProvider>
   );
 };
