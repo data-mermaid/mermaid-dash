8e88fea293a488f248b4332450b5dbf941d44f17
tienhoah <nickhoang11@gmail.com>
2022-11-17 12:27:07 -0800

    Fix bleaching categories bug.

--- src/lib/chart-helpers.js
+++ src/lib/chart-helpers.js
@@ -2,10 +2,17 @@ import { bleachingCategories } from '../constants/transect-protocols';
 
 export default function getChartContent(content, isBleaching) {
   if (isBleaching) {
-    return bleachingCategories.map(({ name, type }) => {
-      return { x: name, y: content[type] };
-    });
+    return bleachingCategories.reduce((accumulator, category) => {
+      const { name, type } = category;
+
+      if (content[type]) {
+        accumulator.push({ x: name, y: content[type] });
+      }
+
+      return accumulator;
+    }, []);
   }
+
   return Object.entries(content).map(([attribute, value]) => {
     return { x: attribute, y: value };
   });
